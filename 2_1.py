93bc33a862a56b3acbfd68e76729563550d2fca2
Karthikeya CH <84000816+codenstar@users.noreply.github.com>
2021-05-16 19:46:41 +0530

    Add files via upload

---
+++ 2_1.py
@@ -0,0 +1,7 @@
+x1=int(input("enter x1 value"))
+y1=int(input("enter y1 value"))
+x2=int(input("enter x2 value"))
+y2=int(input("enter y2 value"))
+d=((x1-x2)**2-(y1-y2)**2)**0.5
+print(d)
+#0.0 is the output
